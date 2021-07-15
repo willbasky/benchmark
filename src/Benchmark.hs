8bbc6679fed6fccb8c6c29df240a83ab7fe4b90e
Vladislav Sabanov <vladislav.sabanov@juspay.in>
2021-07-15 20:10:17 +0500

    Add atto parser and tests

--- src/Benchmark.hs
+++ src/Benchmark.hs
@@ -2,24 +2,60 @@
 
 module Benchmark
     ( patternMatch
+    , patternMatchAtto
     ) where
 
-import qualified Data.Char as C
+-- import qualified Data.Char as C
 import qualified Data.Set as Set
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Attoparsec.Text
+import Control.Applicative
 
 
 -- Text approach
 patternMatch :: Text -> Bool
 patternMatch txt =
-  ((T.unpack txt !! 0) `elem` allowedSymbols) && not (containSpecialChars txt)
+  ((T.unpack txt !! 0) `elem` validCahrs) && not (containSpecialChars txt)
 
 containSpecialChars :: Text -> Bool
 containSpecialChars txt = any isSpecialChar (Set.fromList $ T.unpack txt)
 
 isSpecialChar :: Char -> Bool
-isSpecialChar a = Set.notMember a $ Set.fromList (allowedSymbols ++ ['-','_'])
+isSpecialChar a = Set.notMember a $ Set.fromList (validCahrs ++ ['-','_'])
 
-allowedSymbols :: String
-allowedSymbols = (['a'..'z'] ++ ['A'..'Z'] ++ ['0','1','2','3','4','5','6','7','8','9'])
+validCahrs :: String
+validCahrs = (['a'..'z'] ++ ['A'..'Z'] ++ ['0','1','2','3','4','5','6','7','8','9'])
+
+
+
+-- Attoparsec
+patternMatchAtto:: Text -> Bool
+patternMatchAtto txt = case parseOnly (textParser <* endOfInput) txt of
+  Left _ -> False
+  Right _ -> True
+
+textParser :: Parser Text
+textParser = do
+  f <- validCahrsParser
+  ts <- many1 (try validCharsAndSpecial)
+  pure $ T.pack $ f:ts
+
+validCahrsParser :: Parser Char
+validCahrsParser = try digit <|> try letterL <|> try letterU
+
+validCharsAndSpecial :: Parser Char
+validCharsAndSpecial = try validCahrsParser <|> try special
+
+letterL :: Parser Char
+letterL = satisfy isLetter
+  where isLetter c = c >= 'a' && c <= 'z'
+
+letterU :: Parser Char
+letterU = satisfy isLetter
+  where isLetter c = c >= 'A' && c <= 'Z'
+
+special :: Parser Char
+special = satisfy special
+  where special c = c == '-' || c == '_'
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,12 @@
 module Main (main) where
 
-import Benchmark (patternMatch)
+import Benchmark (patternMatch, patternMatchAtto)
 import Test.Hspec
 
 
 main :: IO ()
-main = hspec $
-	describe "patternMatch" $ do
+main = hspec $ do
+	describe "patternMatch Text" $ do
 		it "Success with Test 1, all regex check successful" $ do
 			patternMatch "9002876543___1234" `shouldBe` True
 		it "Success with Test 2, all regex check successful" $ do
@@ -39,3 +39,37 @@ main = hspec $
 			patternMatch "awertyuijhgfdertyuioiuy" `shouldBe` True
 		it "Success with Test 16, all regex check successful" $ do
 			patternMatch "---------------------------------------" `shouldBe` False
+
+	describe "patternMatchAtto Atto" $ do
+		it "Success with Test 1, all regex check successful" $ do
+			patternMatchAtto "9002876543___1234" `shouldBe` True
+		it "Success with Test 2, all regex check successful" $ do
+			patternMatchAtto "AAb9002876-543___1234" `shouldBe` True
+		it "Success with Test 3, all regex check successful" $ do
+			patternMatchAtto "@-AAb9002876-543___1234" `shouldBe` False
+		it "Success with Test 4, all regex check successful" $ do
+			patternMatchAtto "---AAb9002+876-543___1234" `shouldBe` False
+		it "Success with Test 5, all regex check successful" $ do
+			patternMatchAtto "AAb9002+876-543___1234" `shouldBe` False
+		it "Success with Test 6, all regex check successful" $ do
+			patternMatchAtto "AAb900-OPadff2876-543___1234" `shouldBe` True
+		it "Success with Test 7, all regex check successful" $ do
+			patternMatchAtto "AAb900-OPadff2876-543___1234-----_______" `shouldBe` True
+		it "Success with Test 8, all regex check successful" $ do
+			patternMatchAtto "AAb900-OPadff2876-543___1234-----_______*" `shouldBe` False
+		it "Success with Test 9, all regex check successful" $ do
+			patternMatchAtto "900-O_Pad-_ff2-876-543___1234-----______JAULOqw" `shouldBe` True
+		it "Success with Test 10, all regex check successful" $ do
+			patternMatchAtto "AAb-90111&80-O_Pad-_ff2-876-543___1234-----______JAUL;/\\'Oqw" `shouldBe` False
+		it "Success with Test 11, all regex check successful" $ do
+			patternMatchAtto "A80-O_Pad-_ff2-876-543___1234-----______JAUL\\'Oqw" `shouldBe` False
+		it "Success with Test 12, all regex check successful" $ do
+			patternMatchAtto "A80-O_Pad-_ff2-876-543___1234-----______JAUL\\'\\Oqw" `shouldBe` False
+		it "Success with Test 13, all regex check successful" $ do
+			patternMatchAtto "AASDFGHJERTYUCFGHJTYU" `shouldBe` True
+		it "Success with Test 14, all regex check successful" $ do
+			patternMatchAtto "12345678098765434567" `shouldBe` True
+		it "Success with Test 15, all regex check successful" $ do
+			patternMatchAtto "awertyuijhgfdertyuioiuy" `shouldBe` True
+		it "Success with Test 16, all regex check successful" $ do
+			patternMatchAtto "---------------------------------------" `shouldBe` False
