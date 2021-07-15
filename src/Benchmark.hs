0882aaf7f71d723d1da6b675446c9dffaaec82d5
Vladislav Sabanov <vladislav.sabanov@juspay.in>
2021-07-15 19:17:25 +0500

    Add text implementation

--- src/Benchmark.hs
+++ src/Benchmark.hs
@@ -1,15 +1,25 @@
-{- |
-Copyright: (c) 2021 metaxis
-SPDX-License-Identifier: Apache-2.0
-Maintainer: metaxis <email>
 
-See README for more info
--}
 
 module Benchmark
-    ( projectName
+    (
     ) where
 
+import qualified Data.Char as C
+import qualified Data.Set as Set
+import Data.Text (Text)
+import qualified Data.Text as T
 
-projectName :: String
-projectName = "benchmark"
+
+-- Text approach
+patternMatch :: Text -> Bool
+patternMatch txt =
+  ((T.unpack txt !! 0) `elem` allowedSymbols) && not (containSpecialChars txt)
+
+containSpecialChars :: Text -> Bool
+containSpecialChars txt = any isSpecialChar (Set.fromList $ T.unpack txt)
+
+isSpecialChar :: Char -> Bool
+isSpecialChar a = Set.notMember a $ Set.fromList (allowedSymbols ++ ['-','_'])
+
+allowedSymbols :: String
+allowedSymbols = (['a'..'z'] ++ ['A'..'Z'] ++ ['0','1','2','3','4','5','6','7','8','9'])
